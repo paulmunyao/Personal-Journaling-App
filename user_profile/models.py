ee0be3daa58584fb85db12662ed4d46aba7ac7b7
paulmunyao <paulmunyao094@gmail.com>
2024-07-09 00:48:21 +0300

    run update migrations on user profile

--- user_profile/models.py
+++ user_profile/models.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
-    # picture = models.ImageField(upload_to='profile_pics', blank=True)
+    picture = models.ImageField(upload_to='profile_pics', blank=True)
     location = models.CharField(max_length=100, blank=True)
     birth_date = models.DateField(null=True, blank=True)

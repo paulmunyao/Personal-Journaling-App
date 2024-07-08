1421467f3e44e2adf2576ff4af3134b7a1101f2a
paulmunyao <paulmunyao094@gmail.com>
2024-07-08 16:33:44 +0300

    add django filters

--- Journaling_App/settings.py
+++ Journaling_App/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'user_app',
     'user_entry',
     'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
+
--- user_entry/api/views.py
+++ user_entry/api/views.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from user_entry.models import JournalEntry
@@ -7,6 +8,8 @@ class JournalEntryViewSet(viewsets.ModelViewSet):
     queryset = JournalEntry.objects.all()
     serializer_class = JournalEntrySerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['categories']
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = JournalEntry.objects.all()

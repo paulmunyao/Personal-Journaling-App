149978814c3e5ddbcc0cc6ec657a96011f41e971
paulmunyao <paulmunyao094@gmail.com>
2024-07-09 11:42:31 +0300

    remove cached files

--- user_entry/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from user_entry.models import JournalEntry, Category, Summary
-
-admin.site.register(JournalEntry)
-admin.site.register(Category)
-admin.site.register(Summary)

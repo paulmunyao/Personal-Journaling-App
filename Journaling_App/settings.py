0cb9ffd2a03a9e6487a35f1e3089591eb479478b
paulmunyao <paulmunyao094@gmail.com>
2024-07-09 03:18:53 +0300

    update user profile token

--- Journaling_App/settings.py
+++ Journaling_App/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'user_app',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'Journaling_App.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'journal',
+        'USER': 'journalapp',
+        'PASSWORD': 'SKIPPERMANCITY1998!',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 

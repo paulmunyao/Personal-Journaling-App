be47762826a5499317d5b51a31043567dbc7b7e6
paulmunyao <paulmunyao094@gmail.com>
2024-07-09 18:11:50 +0300

    document api

--- Journaling_App/settings.py
+++ Journaling_App/settings.py
@@ -39,7 +39,16 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'user_app',
     'rest_framework',
+    'drf_yasg',
 ]
+
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'Basic': {
+            'type': 'basic'
+        }
+    }
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
